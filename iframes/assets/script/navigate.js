d4965ca3b998907fb3929f623cbdf8b89a141b55
Chaitanya Annam <132034829+chaitu2003@users.noreply.github.com>
2025-06-13 12:23:30 +0530

    Add files via upload

---
+++ iframes/assets/script/navigate.js
@@ -0,0 +1,20 @@
+function ifclick() {
+  document.getElementById("displayif").innerHTML =
+    '<iframe src="../bmi/index.html" width="100%" height="470" style="border: none" ></iframe>';
+}
+function Portfolio() {
+  document.getElementById("displayif").innerHTML =
+    ' <iframe src="../portfolio/index.html" width="100%" height="470" style="border: none" ></iframe> ';
+}
+function heart() {
+  document.getElementById("displayif").innerHTML =
+    '<iframe src="../heart_disease_risk/index.html" width="100%" height="470" style="border: none"></iframe> ';
+}
+function tool() {
+  document.getElementById("displayif").innerHTML =
+    '<iframe src="../basic_diagnostic_tool/index.html" width="100%" height="470" style="border: none"></iframe> ';
+}
+function lab() {
+  document.getElementById("displayif").innerHTML =
+    '<iframe src="../lab_test_results/index.html" width="100%" height="470" style="border: none"></iframe> ';
+}
